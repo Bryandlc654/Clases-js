80c3870f23902e7cd7d9549bf23e919420fa26cd
Bryandlc654 <bdelacruz654@gmail.com>
2024-01-23 08:38:20 -0500

    Navbar terminado

---
+++ componentes/navbar/script.js
@@ -0,0 +1,5 @@
+function toggleDropdown() {
+    var dropdownList = document.getElementById('dropdownList');
+    dropdownList.style.display = (dropdownList.style.display === 'none' || dropdownList.style.display === '') ? 'block' : 'none';
+}
+
