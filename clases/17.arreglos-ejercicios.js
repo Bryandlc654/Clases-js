71dc3baf7da7a9c77fd4b86cc280c0f7225e1013
Bryandlc654 <bdelacruz654@gmail.com>
2024-01-25 08:35:25 -0500

    19 arreglos

--- clases/17.arreglos-ejercicios.js
+++ clases/17.arreglos-ejercicios.js
@@ -6,6 +6,7 @@ console.log(duplicado);
 /* Uso del Filter */
 let numerosN = [1, 2, 3, 4, 5, 6, 7, 8];
 let numerosPares = numerosN.filter(numero => numero % 2 === 0);
+
 console.log(numerosPares);
 
 /* Uso del Reduce */
